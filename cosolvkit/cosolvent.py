e9661a1cef2b06e1a4af1b43ca28b559afc71ce2
jeeberhardt <qksoneo@gmail.com>
2020-04-17 14:59:39 -0700

    use AMBER standard residues from pdb4amber

--- cosolvkit/cosolvent.py
+++ cosolvkit/cosolvent.py
@@ -15,6 +15,7 @@ import numpy as np
 from openbabel import openbabel as ob
 from rdkit import Chem
 from rdkit.Chem import AllChem
+from pdb4amber.residue import AMBER_SUPPORTED_RESNAMES
 
 from . import utils
 
@@ -30,21 +31,6 @@ check %(resname)s
 saveoff %(resname)s out.lib
 quit
 """
-
-
-AMBER_SUPPORTED_RESNAMES = ('ALA', 'ARG', 'ASN', 'ASP', 'CYS', 'GLN', 'GLU', 'GLY', 'HIS', 
-                            'ILE', 'LEU', 'LYS', 'MET', 'PHE', 'PRO', 'SER', 'THR', 'TRP', 
-                            'TYR', 'VAL', 'HID', 'HIE', 'HIN', 'HIP', 'CYX', 'ASH', 'GLH', 
-                            'LYH', 'ACE', 'NME', 'GL4', 'AS4', 'C', 'G', 'U', 'A', 'DC', 'DG', 
-                            'DT', 'DA', 'OHE', 'C5', 'G5', 'U5', 'A5', 'C3', 'G3', 'U3', 'A3', 
-                            'DC5', 'DG5', 'DT5', 'DA5', 'DC3', 'DG3', 'DT3', 'DA3', 'WAT', 
-                            'HOH', 'AG', 'AL', 'Ag', 'BA', 'BR', 'Be', 'CA', 'CD', 'CE', 'CL', 
-                            'CO', 'CR', 'CS', 'CU', 'CU1', 'Ce', 'Cl-', 'Cr', 'Dy', 'EU', 'EU3', 
-                            'Er', 'F', 'FE', 'FE2', 'GD3', 'HE+', 'HG', 'HZ+', 'Hf', 'IN', 'IOD', 
-                            'K', 'K+', 'LA', 'LI', 'LU', 'MG', 'MN', 'NA', 'NH4', 'NI', 'Na+', 
-                            'Nd', 'PB', 'PD', 'PR', 'PT', 'Pu', 'RB', 'Ra', 'SM', 'SR', 'Sm', 
-                            'Sn', 'TB', 'TL', 'Th', 'Tl', 'Tm', 'U4+', 'V2+', 'Y', 'YB2', 'ZN', 'Zr')
-
 
 
 def _run_antechamber(mol2_filename, molecule_name, resname, charge=0, charge_method="bcc", gaff_version="gaff2"):
