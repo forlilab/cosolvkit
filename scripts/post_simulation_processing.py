8e5c4bbc99a7d53b18f84da9045d000235454cf5
Niccolo Bruciaferri <nbruciaferri@gmail.com>
2024-02-01 17:53:56 -0800

    Working on the autocorrelation plots

--- scripts/post_simulation_processing.py
+++ scripts/post_simulation_processing.py
@@ -12,6 +12,18 @@ import matplotlib.pyplot as plt
 
 from cosolvkit.cosolvent_system import CoSolvent
 
+def bo(data):
+    from pymbar import timeseries
+    from pymbar.timeseries import statistical_inefficiency_multiple, subsample_correlated_data
+    t0, g, Neff_max = timeseries.detect_equilibration(data) # compute indices of uncorrelated timeseries
+    # A_t_equil = data[t0:]
+    A_t_equil = data
+    indices = timeseries.subsample_correlated_data(A_t_equil, g=g)
+    A_n = A_t_equil[indices]
+    print(f't0 is {t0} and indices are {len(indices)} frames')
+    print(f't0 is {t0} and A_t_equil is {len(A_t_equil)} frames')
+    return A_n
+
 def get_temp_vol_pot(log_file):
     df = pd.read_csv(log_file)
     pot_e = list(df["Potential Energy (kJ/mole)"])
@@ -48,7 +60,7 @@ def plot_temp_vol_pot(pot_e, temp, vol, outpath=None):
     )
     if outpath is not None:
         plt.savefig(outpath)
-    # plt.show()
+    plt.close()
     return
 
 def rdf_mda(traj: str, top: str, cosolvents: list, outpath=None, n_frames=250):
@@ -59,7 +71,8 @@ def rdf_mda(traj: str, top: str, cosolvents: list, outpath=None, n_frames=250):
         wat_resname = "WAT"
     oxygen_atoms = u.select_atoms(f"resname {wat_resname} and name O")
     sim_frames = len(u.trajectory)
-        
+    n_bins = sim_frames
+    irdf_results = {}
     for cosolvent in cosolvents:
         cosolvent_name = cosolvent.resname
         r_max = 15
@@ -74,8 +87,9 @@ def rdf_mda(traj: str, top: str, cosolvents: list, outpath=None, n_frames=250):
             plt.tight_layout(pad=2.0)
             # Here compute RDF between same atoms and different molecules
             atoms = cosolvent_residues.select_atoms(f'name {cosolvent_atom}')
-            irdf = rdf.InterRDF(atoms, atoms, nbins=100, range=(0.0, r_max), exclusion_block=(1, 1))
+            irdf = rdf.InterRDF(atoms, atoms, nbins=n_bins, range=(0.0, r_max), exclusion_block=(1, 1))
             irdf.run(start=0, step=1000)
+            irdf_results[(cosolvent_name, cosolvent_atom, cosolvent_name, cosolvent_atom)] = irdf.results.rdf
             # irdf.run()
             ax[0][0].plot(irdf.results.bins, irdf.results.rdf, label="RDF", alpha=0.5)
             ax[0][0].set_xlabel(r'$r$ $\AA$')
@@ -83,7 +97,7 @@ def rdf_mda(traj: str, top: str, cosolvents: list, outpath=None, n_frames=250):
             ax[0][0].set_title(f"RDF-{cosolvent_name} {cosolvent_atom} every 1k frames")
             ax[0][0].legend()
 
-            irdf = rdf.InterRDF(atoms, atoms, nbins=100, range=(0.0, r_max), exclusion_block=(1, 1))
+            irdf = rdf.InterRDF(atoms, atoms, nbins=n_bins, range=(0.0, r_max), exclusion_block=(1, 1))
             irdf.run(start=0, stop=250)
             ax[1][0].plot(irdf.results.bins, irdf.results.rdf, label="RDF", alpha=0.5)
             ax[1][0].set_xlabel(r'$r$ $\AA$')
@@ -92,7 +106,7 @@ def rdf_mda(traj: str, top: str, cosolvents: list, outpath=None, n_frames=250):
             ax[1][0].legend()
             
             # last frames
-            irdf = rdf.InterRDF(atoms, atoms, nbins=100, range=(0.0, r_max), exclusion_block=(1, 1))
+            irdf = rdf.InterRDF(atoms, atoms, nbins=n_bins, range=(0.0, r_max), exclusion_block=(1, 1))
             irdf.run(start=sim_frames-n_frames, stop=sim_frames)
             ax[2][0].plot(irdf.results.bins, irdf.results.rdf, label="RDF", alpha=0.5)
             ax[2][0].set_xlabel(r'$r$ $\AA$')
@@ -101,8 +115,9 @@ def rdf_mda(traj: str, top: str, cosolvents: list, outpath=None, n_frames=250):
             ax[2][0].legend()
             
             # Here compute RDF between atom and water's oxygen
-            irdf = rdf.InterRDF(atoms, oxygen_atoms, nbins=100, range=(0.0, r_max))
+            irdf = rdf.InterRDF(atoms, oxygen_atoms, nbins=n_bins, range=(0.0, r_max))
             irdf.run(start=0, step=1000)
+            irdf_results[(cosolvent_name, cosolvent_atom, "HOH", "O")] = irdf.results.rdf
             # irdf.run()
             ax[0][1].plot(irdf.results.bins, irdf.results.rdf, label="RDF", alpha=0.5)
             ax[0][1].set_xlabel(r'$r$ $\AA$')
@@ -110,7 +125,7 @@ def rdf_mda(traj: str, top: str, cosolvents: list, outpath=None, n_frames=250):
             ax[0][1].set_title(f"RDF {cosolvent_name} {cosolvent_atom}-HOH O every 1k frames")
             ax[0][1].legend()
 
-            irdf = rdf.InterRDF(atoms, oxygen_atoms, nbins=100, range=(0.0, r_max))
+            irdf = rdf.InterRDF(atoms, oxygen_atoms, nbins=n_bins, range=(0.0, r_max))
             irdf.run(start=0, stop=250)
             ax[1][1].plot(irdf.results.bins, irdf.results.rdf, label="RDF", alpha=0.5)
             ax[1][1].set_xlabel(r'$r$ $\AA$')
@@ -119,7 +134,7 @@ def rdf_mda(traj: str, top: str, cosolvents: list, outpath=None, n_frames=250):
             ax[1][1].legend()
             
             # last frames
-            irdf = rdf.InterRDF(atoms, oxygen_atoms, nbins=100, range=(0.0, r_max))
+            irdf = rdf.InterRDF(atoms, oxygen_atoms, nbins=n_bins, range=(0.0, r_max))
             irdf.run(start=sim_frames-n_frames, stop=sim_frames)
             ax[2][1].plot(irdf.results.bins, irdf.results.rdf, label="RDF", alpha=0.5)
             ax[2][1].set_xlabel(r'$r$ $\AA$')
@@ -139,7 +154,7 @@ def rdf_mda(traj: str, top: str, cosolvents: list, outpath=None, n_frames=250):
     r_max = 8.5
     fig, ax = plt.subplots()
     plt.setp(ax, ylim=(0, 4.5), xlim=(0, r_max+1))
-    irdf = rdf.InterRDF(oxygen_atoms, oxygen_atoms, nbins=300, range=(0.0, r_max), exclusion_block=(1, 1))
+    irdf = rdf.InterRDF(oxygen_atoms, oxygen_atoms, nbins=n_bins, range=(0.0, r_max), exclusion_block=(1, 1))
     irdf.run(start=0, step=50)
     # irdf.run()
     ax.plot(irdf.results.bins, irdf.results.rdf, label="RDF", alpha=0.5)
@@ -149,6 +164,47 @@ def rdf_mda(traj: str, top: str, cosolvents: list, outpath=None, n_frames=250):
     ax.legend()
     if outpath is not None:
         plt.savefig(f"{outpath}/rdf_HOH_O.png")
+    plt.close()
+    irdf_results[("HOH", "O", "HOH", "O")] = irdf.results.rdf
+    return irdf_results
+
+def autocorrelation(data):
+    from scipy.signal import correlate
+    
+    """Autocorrelation function"""
+    n = len(data)
+    mean = np.mean(data)
+    autocorr = correlate(data - mean, data - mean, mode='full', method='auto')
+    return autocorr[n - 1:]
+
+def plot_autocorrelation(data, cosolvent_name1=None, cosolvent_atom1=None, cosolvent_name2=None, cosolvent_atom2=None, outpath=""):
+    equilibration = cosolvent_name1 is None and cosolvent_atom1 is None and cosolvent_name2 is None and cosolvent_atom2 is None
+    if equilibration:
+        figname = f"{outpath}/ac_equilibration.png"
+        title = f"Equilibration Auto correlation"
+    else:
+        figname = f"{outpath}/ac_{cosolvent_name1}_{cosolvent_atom1}_{cosolvent_name2}_{cosolvent_atom2}.png"
+        title = f"{cosolvent_name1} {cosolvent_atom1}-{cosolvent_name2} {cosolvent_atom2}"
+    if equilibration:
+        labels = ["Potential Energy", "Volume", "Temperature"]
+        colors = ["g", "b", "r"]
+        for d in range(len(data)):
+            autocorr_values = autocorrelation(data[d])
+            # Normalize autocorrelation values for better plotting
+            normalized_autocorr = autocorr_values / np.max(np.abs(autocorr_values))
+            lags = np.arange(0, len(autocorr_values))
+            plt.stem(lags, normalized_autocorr, colors[d], markerfmt='o', label=labels[d])
+    else:
+        autocorr_values = autocorrelation(data)
+        # Normalize autocorrelation values for better plotting
+        normalized_autocorr = autocorr_values / np.max(np.abs(autocorr_values))
+        lags = np.arange(0, len(autocorr_values))
+        plt.stem(lags, normalized_autocorr, basefmt="k-")
+    plt.title(title)
+    plt.legend()
+    plt.xlabel('Lag')
+    plt.ylabel('Autocorrelation')
+    plt.savefig(figname)
     plt.close()
     return
 
@@ -181,9 +237,29 @@ if __name__ == "__main__":
     cosolvents = list()
     for cosolvent in cosolvents_d:
         cosolvents.append(CoSolvent(**cosolvent))
+    rdf_path = os.path.join(out_path, "rdf")
+    ac_path = os.path.join(out_path, "autocorrelation")
     if not os.path.exists(out_path):
         os.makedirs(out_path)
+    if not os.path.exists(rdf_path):
+        os.makedirs(rdf_path)
+    if not os.path.exists(ac_path):
+        os.makedirs(ac_path)
 
+    # Equilibration
     pot_e, temp, vol = get_temp_vol_pot(log_file)
     plot_temp_vol_pot(pot_e, temp, vol, outpath=out_path+"/equilibration_plot.png")
-    rdf_mda(traj_file, top_file, cosolvents, out_path)
+    
+    irdf_results = rdf_mda(traj_file, top_file, cosolvents, rdf_path)
+    for pair in irdf_results:
+        cosolvent_name1, cosolvent_atom1, cosolvent_name2, cosolvent_atom2 = pair
+        plot_autocorrelation(data=irdf_results[pair],
+                             cosolvent_name1=cosolvent_name1,
+                             cosolvent_atom1=cosolvent_atom1,
+                             cosolvent_name2=cosolvent_name2,
+                             cosolvent_atom2=cosolvent_atom2,
+                             outpath=ac_path)
+    plot_autocorrelation([pot_e, vol, temp], outpath=ac_path)
+    # plot_autocorrelation(data=pot_e, parameter="Potential Energy", outpath=ac_path)
+    # plot_autocorrelation(data=temp, parameter="Temperature (K)", outpath=ac_path)
+    # plot_autocorrelation(data=vol, parameter="Volume", outpath=ac_path)
