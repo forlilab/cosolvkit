26c5732937ccd99ebdd179ee9b311ba6edbea4f1
Niccolo Bruciaferri <nbruciaferri@gmail.com>
2024-02-07 10:59:58 -0800

    Small bugfix in run_simulation from amber files

--- cosolvkit/simulation.py
+++ cosolvkit/simulation.py
@@ -1,7 +1,9 @@
+import os
 from sys import stdout
-from openmm.app import *
-from openmm import *
+import openmm.app as app
+import openmm
 import openmm.unit as openmmunit
+from mdtraj.reporters import NetCDFReporter, DCDReporter
 
 def run_simulation( simulation_format: str = 'OPENMM',
                     results_path: str = "output",
@@ -27,67 +29,72 @@ def run_simulation( simulation_format: str = 'OPENMM',
     if simulation_format.upper() != "OPENMM":
         assert topology is not None and positions is not None, "If the simulation format specified is not OpenMM be sure to pass both topology and positions files"
         if simulation_format.upper() == "AMBER":
-            topology = AmberPrmtopFile(topology)
-            positions = AmberInpcrdFile(positions)
+            positions = app.AmberInpcrdFile(positions)
+            topology = app.AmberPrmtopFile(topology, periodicBoxVectors=positions.boxVectors)
         elif simulation_format.upper() == "GROMACS":
-            positions = GromacsGroFile(positions)
-            topology = GromacsTopFile(topology, periodicBoxVectors=positions.getPeriodicBoxVectors())
+            positions = app.GromacsGroFile(positions)
+            topology = app.GromacsTopFile(topology, periodicBoxVectors=positions.getPeriodicBoxVectors())
         elif simulation_format.upper() == "CHARMM":
-            topology = CharmmPsfFile(topology)
-            positions = CharmmCrdFile(positions)
-        system = topology.createSystem(nonbondedMethod=PME,
+            topology = app.CharmmPsfFile(topology)
+            positions = app.CharmmCrdFile(positions)
+        system = topology.createSystem(nonbondedMethod=app.PME,
                                        nonbondedCutoff=10*openmmunit.angstrom,
-                                       constraints=HBonds,
+                                       constraints=app.HBonds,
                                        hydrogenMass=1.5*openmmunit.amu)
     else:
         assert pdb is not None and system is not None, "If the simulation format specified is OpenMM be sure to pass both pdb file and system.xml"
-        pdb = PDBFile(f'{results_path}/system.pdb')
+        pdb = app.PDBFile(f'{results_path}/system.pdb')
         topology = pdb.topology
         positions = pdb.positions
-        system = XmlSerializer.deserialize(open(f'{results_path}/system.xml').read())
+        system = openmm.XmlSerializer.deserialize(open(f'{results_path}/system.xml').read())
 
     print('Selecting simulation platform')
     try:
-        platform = Platform.getPlatformByName("CUDA")
+        platform = openmm.Platform.getPlatformByName("CUDA")
         platform.setPropertyDefaultValue('DeterministicForces', 'true')
         platform.setPropertyDefaultValue('CudaPrecision', 'mixed')
         platform.setPropertyDefaultValue('CudaDeviceIndex', '0')
         print('Using GPU:CUDA')
     except: 
         try:
-            platform = Platform.getPlatformByName("OpenCL")
+            platform = openmm.Platform.getPlatformByName("OpenCL")
             platform.setPropertyDefaultValue('DeterministicForces', 'true')
             platform.setPropertyDefaultValue('Precision', 'mixed')
             print('Using GPU:OpenCL')
         except:
-            platform = Platform.getPlatformByName("CPU")
+            platform = openmm.Platform.getPlatformByName("CPU")
             print("Switching to CPU, no GPU available.")
 
-    integrator = LangevinMiddleIntegrator(Tstart * openmmunit.kelvin,
+    integrator = openmm.LangevinMiddleIntegrator(Tstart * openmmunit.kelvin,
                                           1 / openmmunit.picosecond,
                                           0.001 * openmmunit.picosecond)
     if seed is not None:
         integrator.setRandomNumberSeed(seed)
         
-    simulation = Simulation(topology, system, integrator, platform)
+    simulation = app.Simulation(topology, system, integrator, platform)
 
     print('Adding reporters to the simulation')
     #every 0.1ns
-    simulation.reporters.append(StateDataReporter(os.path.join(results_path, "statistics.csv"), 25000, step=True, time=True,
+    simulation.reporters.append(app.StateDataReporter(os.path.join(results_path, "statistics.csv"), 25000, step=True, time=True,
                                                 totalEnergy=True, potentialEnergy=True, kineticEnergy=True, 
                                                 temperature=True, volume=True, density=True,
                                                 progress=True, remainingTime=True, speed=True, totalSteps=total_steps))
     #every 0.1ns
-    simulation.reporters.append(StateDataReporter(stdout, 25000, step=True, time=True,
+    simulation.reporters.append(app.StateDataReporter(stdout, 25000, step=True, time=True,
                                                 totalEnergy=True, potentialEnergy=True, kineticEnergy=True, 
                                                 temperature=True, volume=True, density=True,
                                                 progress=True, remainingTime=True, speed=True, totalSteps=total_steps, separator='\t'))
     
     #every 0.1ns
-    simulation.reporters.append(DCDReporter(os.path.join(results_path, "trajectory.dcd"),
-                                            reportInterval=25000, enforcePeriodicBox=None))
+    # simulation.reporters.append(app.DCDReporter(os.path.join(results_path, "trajectory.dcd"),
+    #                                         reportInterval=25000, enforcePeriodicBox=None))
+    simulation.reporters.append(DCDReporter(os.path.join(results_path, "trajectory.dcd"), reportInterval=25000))
+    
+    # simulation.reporters.append(NetCDFReporter(os.path.join(results_path, "trajectory.nc"),
+    #                                         reportInterval=25000))
+    
     #every 1ns
-    simulation.reporters.append(CheckpointReporter(os.path.join(results_path,"simualtion.chk"), 250000)) 
+    simulation.reporters.append(app.CheckpointReporter(os.path.join(results_path,"simualtion.chk"), 250000)) 
 
     print("Setting positions for the simulation")
     try:
@@ -107,17 +114,17 @@ def run_simulation( simulation_format: str = 'OPENMM',
     simulation.context.setVelocitiesToTemperature(Tstart)
     
     # Warm up the system gradually
-    for i in range(nT):
-        temperature = Tstart + i * Tstep
-        integrator.setTemperature(temperature)
-        print(f"Temperature set to {temperature} K.")
-        simulation.step(int(warming_steps / nT))
+    # for i in range(nT):
+    #     temperature = Tstart + i * Tstep
+    #     integrator.setTemperature(temperature)
+    #     print(f"Temperature set to {temperature} K.")
+    #     simulation.step(int(warming_steps / nT))
 
     # Increase the timestep for production simulations
     integrator.setStepSize(0.004 * openmmunit.picoseconds)
 
     print(f'Adding a Montecarlo Barostat to the system')
-    system.addForce(MonteCarloBarostat(1 * openmmunit.bar, Tend * openmmunit.kelvin))
+    system.addForce(openmm.MonteCarloBarostat(1 * openmmunit.bar, Tend * openmmunit.kelvin))
     simulation.context.reinitialize(preserveState=True)
 
     print(f"Running simulation in NPT ensemble for {simulation_steps*0.004/1000} ns")
